76be40023b209a63aeff35a4b92ad4eeb4120f1c
Gustavo Eduardo <202520201@unifoa.edu.br>
2026-04-14 03:19:47 -0300

    Responsividade Perfil

--- js/perfil.js
+++ js/perfil.js
@@ -12,3 +12,28 @@
              let menu = document.getElementById('itens');
              menu.classList.toggle('aberto');
             }
+
+      /* --- 3. MENU HAMBURGER --- */
+
+      const btnMenu = document.getElementById('btnMenu');
+      const btnFechar = document.getElementById('btnFechar');
+      const menuLateral = document.getElementById('menuLateral');
+
+      if (btnMenu && btnFechar && menuLateral) {
+      
+        btnMenu.addEventListener('click', () => {
+          menuLateral.classList.add('menu-aberto');
+        });
+
+      
+        btnFechar.addEventListener('click', () => {
+          menuLateral.classList.remove('menu-aberto');
+        });
+
+       
+        menuLateral.querySelectorAll('a').forEach(link => {
+          link.addEventListener('click', () => {
+            menuLateral.classList.remove('menu-aberto');
+          });
+        });
+      }
