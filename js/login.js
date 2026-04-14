554465dabdf0beb6723e15c971a2de296e59da16
Gustavo Eduardo <202520201@unifoa.edu.br>
2026-04-14 02:59:51 -0300

    Responsividade Login

--- js/login.js
+++ js/login.js
@@ -55,3 +55,28 @@
         input.classList.remove("input-invalido");
         span.style.display = "none";
       }
+
+      /* --- 3. MENU HAMBURGER --- */
+
+      const btnMenu = document.getElementById('btnMenu');
+      const btnFechar = document.getElementById('btnFechar');
+      const menuLateral = document.getElementById('menuLateral');
+
+      if (btnMenu && btnFechar && menuLateral) {
+      
+        btnMenu.addEventListener('click', () => {
+          menuLateral.classList.add('menu-aberto');
+        });
+
+      
+        btnFechar.addEventListener('click', () => {
+          menuLateral.classList.remove('menu-aberto');
+        });
+
+       
+        menuLateral.querySelectorAll('a').forEach(link => {
+          link.addEventListener('click', () => {
+            menuLateral.classList.remove('menu-aberto');
+          });
+        });
+      }
